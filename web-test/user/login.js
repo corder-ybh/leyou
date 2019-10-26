f678a524b5b5340bdc25d288ae2e2061b95105a5
2270933604@qq.com <2270933604@qq.com>
2019-10-26 12:24:32 +0800

    使用vue-router

---
+++ web-test/user/login.js
@@ -0,0 +1,8 @@
+const loginForm = {
+    template: '' +
+        '<div>' +
+        '<h2>登陆页</h2>' +
+        '用户名: <input type="text"> <br>' +
+        '密码: <input type="password"> <br>' +
+        '</div>'
+}
