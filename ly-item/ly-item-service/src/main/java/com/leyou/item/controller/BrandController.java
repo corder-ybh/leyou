3faff6fbd870b502f12381bd6622f58a39d90f36
2270933604@qq.com <2270933604@qq.com>
2019-10-27 16:02:26 +0800

    品牌查询接口ok

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/controller/BrandController.java
@@ -0,0 +1,34 @@
+package com.leyou.item.controller;
+
+import com.leyou.common.pojo.PageResult;
+import com.leyou.item.pojo.Brand;
+import com.leyou.item.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("brand")
+public class BrandController {
+    @Autowired
+    private BrandService brandService;
+
+    @GetMapping("page")
+    public ResponseEntity<PageResult<Brand>> queryBrandByPage(
+            @RequestParam(value = "page", defaultValue = "1") Integer page,
+            @RequestParam(value = "rows", defaultValue = "5") Integer rows,
+            @RequestParam(value = "sortBy", required = false) String sortBy,
+            @RequestParam(value = "desc", defaultValue = "false") Boolean desc,
+            @RequestParam(value = "key", required = false) String key) {
+        PageResult<Brand> result =
+                this.brandService.queryBrandByPageAndSort(page, rows, sortBy, desc, key);
+        if (null == result || 0 == result.getItems().size()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(result);
+    }
+}
